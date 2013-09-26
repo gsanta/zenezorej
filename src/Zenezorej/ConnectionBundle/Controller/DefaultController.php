6856d706fbdea316d262ce5a6bbc4c00ab02e50b
gsanta <santa_gergely@hotmail.com>
2013-09-26 15:52:31 +0200

    connection: base forum functionalities are ready: new topic, topic list, messages

--- src/Zenezorej/ConnectionBundle/Controller/DefaultController.php
+++ src/Zenezorej/ConnectionBundle/Controller/DefaultController.php
@@ -3,24 +3,56 @@
 namespace Zenezorej\ConnectionBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Zenezorej\ConnectionBundle\Entity\Login;
+use Zenezorej\ConnectionBundle\Entity\Topic;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request, $topicId)
     {
 
     	// create a task and give it some dummy data for this example
-        $login = new Login();
-        $login->setUserName('Felhasználó név');
-        $login->setPassword('Jelszó');
+        $topic = new Topic();
 
-        $loginForm = $this->createFormBuilder($login)
-            ->add('userName', 'text')
-            ->add('password', 'password')
+        $topicForm = $this->createFormBuilder($topic)
+            ->add('name', 'text')
             ->add('save', 'submit')
             ->getForm();
 
-        return $this->render('ZenezorejConnectionBundle:Default:index.html.twig',array('loginForm' => $loginForm->createView()));
+        $topicForm->handleRequest($request);
+
+        if ($topicForm->isValid()) {
+            
+            $usr= $this->get('security.context')->getToken()->getUser();
+
+            $topic->setUser($usr);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($topic);
+            $em->flush();
+            return $this->redirect($this->generateUrl('connection_homepage'));
+        }
+
+        $topicRepository = $this->getDoctrine()->getRepository('ZenezorejConnectionBundle:Topic');
+
+        $topics = $topicRepository->findAll();
+
+        $messages = array();
+
+        if($topicId != -1) {
+            $messages = $this->fetchAllMessagesByTopicId($topicId);
+        }
+
+        return $this->render('ZenezorejConnectionBundle:Default:index.html.twig',array('topicForm' => $topicForm->createView(), 'topics' => $topics, 'messages' => $messages));
+    }
+
+    private function fetchAllMessagesByTopicId($topicId) {
+        $topicRepository = $this->getDoctrine()->getRepository('ZenezorejConnectionBundle:Topic');
+        $topic = $topicRepository->find($topicId);
+
+        $messageRepository = $this->getDoctrine()->getRepository('ZenezorejConnectionBundle:Message');
+        $messages = $messageRepository->findByTopic($topic);
+
+        return $messages; 
     }
 }
--- src/Zenezorej/ConnectionBundle/Entity/User.php
+++ src/Zenezorej/ConnectionBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace Zenezorej\ConnectionBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Topic
@@ -11,7 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="user")
  * @ORM\Entity
  */
-class User
+class User implements UserInterface, \Serializable
 {
     /**
      * @var integer
@@ -22,15 +23,25 @@ class User
      */
     private $id;
 
-   /**
-     * @ORM\Column(type="string", length=100)
+    /**
+     * @ORM\Column(type="string", length=25, unique=true)
      */
-    private $name;
+    private $username;
 
     /**
-     * @ORM\Column(type="string", length=100)
+     * @ORM\Column(type="string", length=32)
+     */
+    private $salt;
+
+    /**
+     * @ORM\Column(type="string", length=40)
      */
     private $password;
+
+    /**
+     * @ORM\Column(type="string", length=60, unique=true)
+     */
+    private $email;
 
     /**
      * @var string
@@ -57,5 +68,231 @@ class User
     {
         $this->messages = new ArrayCollection();
         $this->topics = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set salt
+     *
+     * @param string $salt
+     * @return User
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+    
+        return $this;
+    }
+
+    /**
+     * Get salt
+     *
+     * @return string 
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set registration_date
+     *
+     * @param \DateTime $registrationDate
+     * @return User
+     */
+    public function setRegistrationDate($registrationDate)
+    {
+        $this->registration_date = $registrationDate;
+    
+        return $this;
+    }
+
+    /**
+     * Get registration_date
+     *
+     * @return \DateTime 
+     */
+    public function getRegistrationDate()
+    {
+        return $this->registration_date;
+    }
+
+    /**
+     * Add messages
+     *
+     * @param \Zenezorej\ConnectionBundle\Entity\Message $messages
+     * @return User
+     */
+    public function addMessage(\Zenezorej\ConnectionBundle\Entity\Message $messages)
+    {
+        $this->messages[] = $messages;
+    
+        return $this;
+    }
+
+    /**
+     * Remove messages
+     *
+     * @param \Zenezorej\ConnectionBundle\Entity\Message $messages
+     */
+    public function removeMessage(\Zenezorej\ConnectionBundle\Entity\Message $messages)
+    {
+        $this->messages->removeElement($messages);
+    }
+
+    /**
+     * Get messages
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getMessages()
+    {
+        return $this->messages;
+    }
+
+    /**
+     * Add topics
+     *
+     * @param \Zenezorej\ConnectionBundle\Entity\Topic $topics
+     * @return User
+     */
+    public function addTopic(\Zenezorej\ConnectionBundle\Entity\Topic $topics)
+    {
+        $this->topics[] = $topics;
+    
+        return $this;
+    }
+
+    /**
+     * Remove topics
+     *
+     * @param \Zenezorej\ConnectionBundle\Entity\Topic $topics
+     */
+    public function removeTopic(\Zenezorej\ConnectionBundle\Entity\Topic $topics)
+    {
+        $this->topics->removeElement($topics);
+    }
+
+    /**
+     * Get topics
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getTopics()
+    {
+        return $this->topics;
+    }
+
+     /**
+     * @inheritDoc
+     */
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function eraseCredentials()
+    {
+    }
+
+    /**
+     * @see \Serializable::serialize()
+     */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+        ));
+    }
+
+    /**
+     * @see \Serializable::unserialize()
+     */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+        ) = unserialize($serialized);
     }
 }
