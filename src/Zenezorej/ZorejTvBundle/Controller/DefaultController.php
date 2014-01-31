a92a820769124193e5bc2cf80ef52d79827c53ac
gsanta <santa_gergely@hotmail.com>
2014-01-31 21:27:00 +0100

    modifed files to previous commit

--- src/Zenezorej/ZorejTvBundle/Controller/DefaultController.php
+++ src/Zenezorej/ZorejTvBundle/Controller/DefaultController.php
@@ -7,17 +7,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     public function indexAction() {
-    	$videos = $this->fetchAllChannels();
+    	$videos = $this->fetchAllVideos();
+    	$channels = $this->fetchAllChannels();
 
-        return $this->render('ZenezorejZorejTvBundle:Default:index.html.twig', array("videos" => $videos));
+        return $this->render('ZenezorejZorejTvBundle:Default:index.html.twig', array("videos" => $videos, "channels" => $channels));
     }
 
-    private function fetchAllChannels() {
+    private function fetchAllVideos() {
 
 	    $videos = $this->getDoctrine()
 	        ->getRepository('ZenezorejZorejTvBundle:Videos')
 	        ->findAll();
 
 	    return $videos;
+	}
+
+	private function fetchAllChannels() {
+		$channels = $this->getDoctrine()
+	        ->getRepository('ZenezorejZorejTvBundle:Channels')
+	        ->findAll();
+
+	    return $channels;
 	}
 }
