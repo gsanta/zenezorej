0587eaec541292728cf714d4bbb308022668dbc9
gsanta <santa_gergely@hotmail.com>
2014-02-01 00:06:47 +0100

    Some files removed

--- src/Zenezorej/SecurityBundle/ZenezorejSecurityBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Zenezorej\SecurityBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ZenezorejSecurityBundle extends Bundle
-{
-}
--- src/Zenezorej/ZorejTvBundle/ZenezorejZorejTvBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Zenezorej\ZorejTvBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ZenezorejZorejTvBundle extends Bundle
-{
-}
